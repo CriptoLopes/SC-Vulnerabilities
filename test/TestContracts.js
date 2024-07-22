25bf6b0789518a85b45f860e2667c6395f1ae429
CriptoLopes <142147333+CriptoLopes@users.noreply.github.com>
2024-07-22 16:44:59 +0200

    Update TestContracts.js

--- test/TestContracts.js
+++ test/TestContracts.js
@@ -1,1 +1,18 @@
+const VulnerableContract = artifacts.require('VulnerableContract');
+const AttackContract = artifacts.require('AttackContract');
+
+contract('VulnerableContract', (accounts) => {
+  it('should deploy contracts', async () => {
+    const vulnerable = await VulnerableContract.deployed();
+    assert(vulnerable.address !== '');
+  });
+
+  it('should allow the attack contract to take ownership', async () => {
+    const vulnerable = await VulnerableContract.deployed();
+    const attack = await AttackContract.new(vulnerable.address);
+    await attack.exploitOwnership();
+    const newOwner = await vulnerable.owner();
+    assert(newOwner === attack.address);
+  });
+});
 
